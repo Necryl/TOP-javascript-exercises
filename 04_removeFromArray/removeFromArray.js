f3d2be4eb4717f2e6ce0c77406483549a138d730
Necryl <necrylthewayfarer@gmail.com>
2022-08-07 19:33:05 +0530

    Exercise 3: removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray) {
+    let index;
 
+    for (let i = 1; i < arguments.length; i++) {
+    
+        index = theArray.findIndex((string) => {
+            if (string === arguments[i]) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+        if (index >= 0) {
+            theArray.splice(index, 1);
+        }
+    }
+
+    return theArray;
 };
 
 // Do not edit below this line
