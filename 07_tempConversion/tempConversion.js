61cd266fb18e1c2e6deb12f898a872b6fc879afd
Necryl <necrylthewayfarer@gmail.com>
2022-08-07 20:24:45 +0530

    Exercise 7: tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  let result = (num - 32) / 9 * 5;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let result = num / 5 * 9 + 32;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
