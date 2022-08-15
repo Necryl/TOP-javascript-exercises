1ad18704cc4d39de20bc19d24b7782a4ce8448a9
Necryl <necrylthewayfarer@gmail.com>
2022-08-15 13:36:04 +0530

    Exercise 9: Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text.replace(/[.,\/#!$%@\^&\*;:{}=\-_`~()]/g,"").replace(/\s/g, '').toLowerCase();
+    let odd = text.length % 2 != 0 ? true:false;
+    
+    let mid = odd ? (text.length - 1)/2:null;
+    let text1 = text.slice(0, odd ? mid:text.length/2);
+    let text2 = text.slice(odd ? mid + 1:text.length/2, text.length)
+    
+    return text1 === Array.from(text2).reduce((total, i) => i + total) ? true:false
 };
 
 // Do not edit below this line
