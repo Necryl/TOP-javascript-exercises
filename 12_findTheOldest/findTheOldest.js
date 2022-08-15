8ae8eb68523bfeefe44427482f0f3877b8ad7098
Necryl <necrylthewayfarer@gmail.com>
2022-08-15 14:43:35 +0530

    Exercise 12: findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let result = array.reduce((result, obj) => {
+        age = (obj.yearOfDeath ? obj.yearOfDeath:new Date().getFullYear()) - obj.yearOfBirth;
+        if (result[0] < age) {
+            return [age, obj];
+        }
+        return result;
+    }, [0, 'obj']);
+    return result[1];
 };
 
 // Do not edit below this line
