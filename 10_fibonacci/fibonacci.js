0ef66b0929517e740ada9af468bd30a6289db199
Necryl <necrylthewayfarer@gmail.com>
2022-08-15 14:15:27 +0530

    Exercise 10: fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof num  === 'string') {
+        num = parseInt(num);
+    }
+    if (num < 1) {
+        return "OOPS";
+    } else if (num < 3) {
+        return 1;
+    }
+    let history = [1, 1];
+    let result;
+    for (let i = 2; i < num; i++) {
+        result = history[0] + history[1];
+        history[0] = history[1];
+        history[1] = result;
+    }
+    return result
 };
 
 // Do not edit below this line
